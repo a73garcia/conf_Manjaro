0ce1e0195596a2f9ffc51d8fb89d29d0434b9305
a73garcia <153382653+a73garcia@users.noreply.github.com>
2024-03-08 16:21:23 +0100

    Update CyberghostVPN.sh

--- CyberGhostVPN/CyberghostVPN.sh
+++ CyberGhostVPN/CyberghostVPN.sh
@@ -1,1 +1,24 @@
-sss
+#CyberghostVPN
+
+#Para que nos muestre una lista de los paises:
+cyberghostvpn --traffic --country-code
+
+#Si queremos que nos muestre una lista de las cuidades de un pais:
+cyberghostvpn --traffic --country-code XX
+
+#Si queremos ver un listado de los servidores que hay en una determinada ciudad:
+cyberghostvpn --traffic --country-code XX --city "CIUDAD"
+
+#----------------------------------------------------------
+
+#Como levantar un tunel VPN:
+sudo cyberghostvpn --traffic --country-code XX --connect
+sudo cyberghostvpn --traffic --country-code XX --city "CIUDAD"  --connect
+sudo cyberghostvpn --traffic --country-code XX --city "CIUDAD" --server SERVIDOR --connect
+
+#Ejemplo de conexion a un servidor concreto de un pais:
+sudo cyberghostvpn --traffic --country-code BS --city "Nassau" --server nassau-s402-i01 --connect
+
+
+#Para poder para la conexion VPN:
+sudo cyberghostvpn --stop
